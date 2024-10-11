a2241cb8684b2155de658ccffa699e07d2dd9165
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-10-10 20:09:22 -0400

    Complete 'Arithmetic Operator' section

---
+++ ArithmeticOperators.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    // arithmetic operators
+
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)   
+
+    int x = 5;
+    int y = 2;
+
+    float z = x / (float) y;
+    
+    int t = x % y;
+
+
+    x++;
+
+    printf("%d", x);
+    // printf("%f", z);
+
+    return 0;
+}
