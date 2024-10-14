db4541eaaf0d5b7d5da3e863113e2ac0189c6c30
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-10-14 17:18:59 -0400

    Complete 'Return Statement' section

---
+++ 13-24/ReturnStatement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+double square(double x){
+    double result = x * x;
+    return result;
+}
+
+int main(){
+
+    // return = returns a value back to a calling function
+
+    double x = square(3.14);
+    printf("%lf", x);
+
+    return 0;
+}
