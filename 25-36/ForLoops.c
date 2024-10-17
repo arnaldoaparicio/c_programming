75e16d29449e595385e7b7689e19ad1ef387a842
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-10-17 16:46:43 -0400

    Complete 'For Loops' section

---
+++ 25-36/ForLoops.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // for loop = repeats a section of code a limited amount of times
+    
+    for(int index = 1; index <= 10; index+=2){
+        printf("%d\n", index);
+    }
+    return 0;
+}
